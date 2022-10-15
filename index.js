1e3d3870bb2ad52a13088bb348cf6e63d0436066
tiagotecna <tiago@tecnasistemas.com.br>
2022-10-15 18:16:28 -0300

    config index, routes and controller

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+const express = require("express");
+const path = require("path");
+const routes = require("./routes/routes");
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes)
+
+
+app.listen(3000, () => console.log(`Servidor rodando em https://localhost:${port}`));
